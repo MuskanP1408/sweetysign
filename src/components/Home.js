e6b4ccc995a737e474aaed9361e647853cd74bed
MuskanP1408 <mprajapati1408@gmail.com>
2024-12-07 00:07:41 +0530

    responsive_changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,21 +19,23 @@ function CounterCard({ title, initialValue, limit }) {
     if (count < limit) {
       const interval = setInterval(() => {
         setCount((prevCount) => prevCount + 1);
-      },5);
+      }, 5);
 
       return () => clearInterval(interval);
     }
   }, [count, limit]);
 
   return (
-    <center><MDBCard className="text-center mb-4 custom-card" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', color: 'white' }}>
-      <MDBCardBody className="custom-card-body">
-        <MDBCardText>
-          <h3>{count}</h3>
-        </MDBCardText>
-        <MDBCardTitle>{title}</MDBCardTitle>
-      </MDBCardBody>
-    </MDBCard></center>
+    <center>
+      <MDBCard className="text-center mb-4 custom-card" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', color: 'white' }}>
+        <MDBCardBody className="custom-card-body">
+          <MDBCardText>
+            <h3>{count}</h3>
+          </MDBCardText>
+          <MDBCardTitle>{title}</MDBCardTitle>
+        </MDBCardBody>
+      </MDBCard>
+    </center>
   );
 }
 
@@ -52,69 +54,51 @@ export default function Home() {
       <div className="bg-image">
         <img
           src="https://mdbootstrap.com/img/new/standard/city/053.webp"
-          className="w-100 h-60"
+          className="w-100"
           alt="Sample"
         />
         <div className="mask" style={{ backgroundColor: 'rgba(0, 0, 0, 0.8)' }}>
           <div className="d-flex justify-content-center align-items-center h-100">
             <MDBContainer>
-              <MDBRow>
-                <MDBCol sm="8">
+              <MDBRow className='logo_name'>
+                <MDBCol sm="12" md="8">
                   <h1>
                     <i
                       className={bounce ? 'bounce' : ''}
                       onMouseEnter={handleMouseEnter} // Trigger bounce on hover
                     >
                       WELCOME TO
-                    </i> <br></br>
+                    </i> 
+                    <br />
                     Your Project Name
                   </h1>
                 </MDBCol>
-                <MDBCol sm="4">
+                <MDBCol sm="12" md="4">
                   <h4>Creatofox is a Comprehensive Marketing Company</h4>
                   <MDBBtn outline color="warning" href="#">
                     Company Profile
                   </MDBBtn>
                 </MDBCol>
               </MDBRow>
-
-              {/* Cards Section */}
-        <MDBContainer className='cardSection'>
-          
-            <MDBRow>
-              <MDBCol sm="12" md="4">
-                <CounterCard title="Years Of Experience" initialValue={0} limit={5} />
-              </MDBCol>
-              <MDBCol sm="12" md="4">
-                <CounterCard title="Project Completed" initialValue={50} limit={150} />
-              </MDBCol>
-              <MDBCol sm="12" md="4">
-                <CounterCard title="Happy Clients" initialValue={150} limit={500} />
-              </MDBCol>
-            </MDBRow>
-          </MDBContainer>
-        
             </MDBContainer>
           </div>
         </div>
       </div>
 
-      {/* Cards Section
-        <MDBContainer>
-          <MDBContainer>
-            <MDBRow>
-              <MDBCol sm="12" md="4">
-                <CounterCard title="Years Of Experience" initialValue={0} limit={5} />
-              </MDBCol>
-              <MDBCol sm="12" md="4">
-                <CounterCard title="Project Completed" initialValue={0} limit={150} />
-              </MDBCol>
-              <MDBCol sm="12" md="4">
-                <CounterCard title="Happy Clients" initialValue={0} limit={500} />
-              </MDBCol>
-            </MDBRow>
-          </MDBContainer>
-        </MDBContainer> */}
-      </div>
+      {/* Cards Section */}
+      <MDBContainer className='cardSection' fluid>
+        <MDBRow>
+          <MDBCol sm="12" md="4">
+            <CounterCard title="Years Of Experience" initialValue={0} limit={5} />
+          </MDBCol>
+          <MDBCol sm="12" md="4">
+            <CounterCard title="Project Completed" initialValue={50} limit={150} />
+          </MDBCol>
+          <MDBCol sm="12" md="4">
+            <CounterCard title="Happy Clients" initialValue={150} limit={500} />
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </div>
   );
 }
